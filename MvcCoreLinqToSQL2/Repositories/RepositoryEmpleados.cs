467ae29bcf46e03fb6c1fcf901c74126222d9ada
jjlagunamoreno <jjlagunamoreno@gmail.com>
2025-02-05 12:42:44 +0100

    Add project files.

---
+++ MvcCoreLinqToSQL2/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,92 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using MvcCoreLinqToSQL.Models;
+
+namespace MvcCoreLinqToSQL.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private DataTable tablaEmpleados;
+        public RepositoryEmpleados()
+        {
+            string connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=sa;Encrypt=True;Trust Server Certificate=True";
+            string sql = "select * from EMP";
+            SqlDataAdapter adEmp = new SqlDataAdapter(sql, connectionString);
+            this.tablaEmpleados = new DataTable();
+            adEmp.Fill(this.tablaEmpleados);
+        }
+
+        public List<Empleado> GetEmpleados()
+        {
+            //LAS CONSULTAS LINQ SE ALMACENAN EN GENERICOS (var) 
+            var consulta = from datos in this.tablaEmpleados.AsEnumerable()
+                           select datos;
+            //AHORA MISMO TENEMOS DENTRO DE CONSULTA LA INFORMACION DE  
+            //LA TABLA EMPLEADOS 
+            //EN ESTE EJEMPLO TENEMOS OBJETO DataRow QUE SON FILAS 
+            //DENTRO DE LA TABLA 
+            //DEBEMOS RECORRER DICHAS FILAS Y EXTRAER LA INFORMACION  
+            //EN OBJETOS DE TIPO Empleado 
+            List<Empleado> empleados = new List<Empleado>();
+            //RECORREMOS CADA FILA DE LA CONSULTA 
+            foreach (var row in consulta)
+            {
+                Empleado emp = new Empleado();
+                //PARA EXTRAER DATOS DE UN DataRow 
+                //DataRow.Field<tipo>("COLUMNA") 
+                emp.IdEmpleado = row.Field<int>("EMP_NO");
+                emp.Apellido = row.Field<string>("APELLIDO");
+                emp.Oficio = row.Field<string>("OFICIO");
+                emp.Salario = row.Field<int>("SALARIO");
+                emp.IdDepartamento = row.Field<int>("DEPT_NO");
+                empleados.Add(emp);
+            }
+            return empleados;
+        }
+        public Empleado FindEmpleado(int idEmpleado)
+        {
+            var consulta = from datos in this.tablaEmpleados.AsEnumerable()
+                           where datos.Field<int>("EMP_NO") == idEmpleado
+                           select datos;
+
+            var row = consulta.FirstOrDefault();
+
+            if (row == null)
+            {
+                return null;
+            }
+
+            Empleado emp = new Empleado
+            {
+                IdEmpleado = row.Field<int>("EMP_NO"),
+                Apellido = row.Field<string>("APELLIDO"),
+                Oficio = row.Field<string>("OFICIO"),
+                Salario = row.Field<int>("SALARIO"),
+                IdDepartamento = row.Field<int>("DEPT_NO")
+            };
+            return emp;
+        }
+
+        public List<Empleado> GetEmpleadosOficioSalario(string oficio, int salario)
+        {
+            var consulta = from datos in this.tablaEmpleados.AsEnumerable()
+                           where datos.Field<string>("OFICIO") == oficio
+&& datos.Field<int>("SALARIO") >= salario
+                           select datos;
+            List<Empleado> empleados = new List<Empleado>();
+            foreach (var row in consulta)
+            {
+                Empleado emp = new Empleado
+                {
+                    IdEmpleado = row.Field<int>("EMP_NO"),
+                    Apellido = row.Field<string>("APELLIDO"),
+                    Oficio = row.Field<string>("OFICIO"),
+                    Salario = row.Field<int>("SALARIO"),
+                    IdDepartamento = row.Field<int>("DEPT_NO")
+                };
+                empleados.Add(emp);
+            }
+            return empleados;
+        }
+    }
+}
