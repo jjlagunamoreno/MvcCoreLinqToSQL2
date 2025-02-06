2b1e63264eee42b64e92bfdb5ef6422c2fe91ab5
jjlagunamoreno <jjlagunamoreno@gmail.com>
2025-02-06 11:44:11 +0100

    buscador y crud doctores sin funcionalidades

---
+++ PracticaFinal/Controllers/DoctoresController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticaFinal.Models;
+using PracticaFinal.Repositories;
+
+namespace PracticaFinal.Controllers
+{
+    public class DoctoresController : Controller
+    {
+        private RepositoryDoctores repo;
+
+        public DoctoresController()
+        {
+            string connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Encrypt=True;Trust Server Certificate=True";
+            this.repo = new RepositoryDoctores(connectionString);
+        }
+
+        public IActionResult Index()
+        {
+            List<Doctor> doctores = this.repo.GetDoctores();
+            return View(doctores);
+        }
+
+        public IActionResult Buscador()
+        {
+            List<string> especialidades = this.repo.GetEspecialidades();
+            ViewData["Especialidades"] = especialidades;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Buscador(string especialidad)
+        {
+            List<string> especialidades = this.repo.GetEspecialidades();
+            ViewData["Especialidades"] = especialidades;
+
+            List<Doctor> doctores = this.repo.GetDoctoresByEspecialidad(especialidad);
+            ViewData["EspecialidadSeleccionada"] = especialidad;
+            return View(doctores);
+        }
+    }
+}
---
+++ PracticaFinal/Repositories/RepositoryDoctores.cs
@@ -0,0 +1,82 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using PracticaFinal.Models;
+
+namespace PracticaFinal.Repositories
+{
+    public class RepositoryDoctores
+    {
+        private string connectionString;
+
+        public RepositoryDoctores(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<Doctor> GetDoctores()
+        {
+            using (SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                string sql = "SELECT * FROM DOCTOR";
+                SqlCommand cmd = new SqlCommand(sql, connection);
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<Doctor> doctores = new List<Doctor>();
+                while (reader.Read())
+                {
+                    doctores.Add(new Doctor
+                    {
+                        HospitalCod = Convert.ToInt32(reader["HOSPITAL_COD"]),
+                        DoctorNo = Convert.ToInt32(reader["DOCTOR_NO"]),
+                        Apellido = reader["APELLIDO"].ToString(),
+                        Especialidad = reader["ESPECIALIDAD"].ToString(),
+                        Salario = Convert.ToInt32(reader["SALARIO"])
+                    });
+                }
+                return doctores;
+            }
+        }
+
+        public List<string> GetEspecialidades()
+        {
+            using (SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                string sql = "SELECT DISTINCT ESPECIALIDAD FROM DOCTOR";
+                SqlCommand cmd = new SqlCommand(sql, connection);
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<string> especialidades = new List<string>();
+                while (reader.Read())
+                {
+                    especialidades.Add(reader["ESPECIALIDAD"].ToString());
+                }
+                return especialidades;
+            }
+        }
+
+        public List<Doctor> GetDoctoresByEspecialidad(string especialidad)
+        {
+            using (SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                string sql = "SELECT * FROM DOCTOR WHERE ESPECIALIDAD = @especialidad";
+                SqlCommand cmd = new SqlCommand(sql, connection);
+                cmd.Parameters.AddWithValue("@especialidad", especialidad);
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<Doctor> doctores = new List<Doctor>();
+                while (reader.Read())
+                {
+                    doctores.Add(new Doctor
+                    {
+                        HospitalCod = Convert.ToInt32(reader["HOSPITAL_COD"]),
+                        DoctorNo = Convert.ToInt32(reader["DOCTOR_NO"]),
+                        Apellido = reader["APELLIDO"].ToString(),
+                        Especialidad = reader["ESPECIALIDAD"].ToString(),
+                        Salario = Convert.ToInt32(reader["SALARIO"])
+                    });
+                }
+                return doctores;
+            }
+        }
+    }
+}
